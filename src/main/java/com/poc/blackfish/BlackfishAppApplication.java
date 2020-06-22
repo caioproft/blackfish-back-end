1bb7fd3b98269415bcd11bf4a0683feae576a40d
Caio Proft <caio_proft@hotmail.com>
2020-06-22 14:43:40 -0300

    :tada: Projeto criado

---
+++ src/main/java/com/poc/blackfish/BlackfishAppApplication.java
@@ -0,0 +1,13 @@
+package com.poc.blackfish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackfishAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackfishAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/blackfish/BlackfishAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.blackfish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackfishAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
